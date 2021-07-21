fc93c067546707b3b0f82e091956aa09a91642e0
nielskrijger <niels@kryger.nl>
2021-07-21 23:00:38 +0200

    docs: improve app service documentation

--- app_service.go
+++ app_service.go
@@ -3,17 +3,26 @@ package goboot
 // AppService instantiates a singleton application service that is created
 // on application boot and shutdown gracefully on application termination.
 type AppService interface {
-	// Configure is run when creating a new app context.
+	// Configure is run when a new app context is created. Use this to load
+	// configuration settings and do initial setup.
+	//
+	// Any error will cause a panic. The app waits for all services to be
+	// Configure'd before calling Init.
 	Configure(ctx *AppContext) error
 
 	// Init is run after all services have been configured. Use this to run
 	// setup that is dependent on other services.
 	//
-	// The app will only start after all initializations are finished.
+	// If the service does not depend on any other services you should do
+	// setup during Configure instead of Init.
+	//
+	// Any error will cause a panic. The app starts after all initializations
+	// are finished.
 	Init() error
 
 	// Close is run right before shutdown. The app waits until close resolves.
-	// Any error is logged.
+	//
+	// Any returned error is logged but will not cause a panic or exit.
 	Close() error
 
 	Name() string
